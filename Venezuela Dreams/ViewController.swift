4dadb79d9bffd57dd385e106fc2e09ea944e21f7
Pascal-Mac <pascalsbakker@gmail.com>
2018-01-16 18:40:52 -0500

    adding xcode project
    Signed-off-by: Pascal-Mac <pascalsbakker@gmail.com>

---
+++ Venezuela Dreams/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Venezuela Dreams
+//
+//  Created by Pascal on 1/16/18.
+//  Copyright © 2018 Venezuela Dreams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
