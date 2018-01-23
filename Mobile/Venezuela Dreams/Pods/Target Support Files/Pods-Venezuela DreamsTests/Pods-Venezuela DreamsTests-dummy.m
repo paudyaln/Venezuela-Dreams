20418900b8ec27eadbe0655f215b3deea1fef750
Pascal-Mac <pascalsbakker@gmail.com>
2018-01-23 14:06:23 -0500

    Deleted Old Project
    Removed old xcode project

--- Mobile/Venezuela Dreams/Pods/Target Support Files/Pods-Venezuela DreamsTests/Pods-Venezuela DreamsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Venezuela_DreamsTests : NSObject
-@end
-@implementation PodsDummy_Pods_Venezuela_DreamsTests
-@end
--- Mobile/Venezuela Dreams/Pods/Target Support Files/Pods-Venezuela DreamsUITests/Pods-Venezuela DreamsUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Venezuela_DreamsUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Venezuela_DreamsUITests
-@end
